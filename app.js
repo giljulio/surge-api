37eb67eae6b35dfcf3f7c977363729eef08805ac
Gil Julio <gilsjulio@gmail.com>
2015-01-20 02:09:09 +0000

    Added swagger to the project

--- app.js
+++ app.js
@@ -3,25 +3,38 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var env = require('node-env-file');
+var swagger = require('swagger-express');
+var path = require('path');
 
-if(!process.env.HEROKU) {
-    env('./.env');
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+if (process.env.NODE_ENV === 'development') {
+    require('node-env-file')('.env');
 }
-
-
-
-mongoose.connect(process.env.MONGOLAB_URI);
-
-var users = require('./routes/users');
 
 var app = express();
 
+app.set('port', process.env.PORT || 3000);
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(swagger.init(app, {
+    apiVersion: '1.0',
+    swaggerVersion: '1.0',
+    basePath: 'http://localhost:3000',
+    swaggerURL: '/api-playground',
+    swaggerJSON: '/api-docs.json',
+    swaggerUI: './public/swagger/',
+    apis: ['./routes/users.js']
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+mongoose.connect(process.env.MONGOLAB_URI);
 
+
+
+
+var users = require('./routes/users');
 app.use('/users', users);
 
 
