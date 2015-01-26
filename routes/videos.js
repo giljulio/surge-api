0a7c58d064d8d1decf27ce607c41354bdb0f3feb
Qasim Shabir <Qasim-786-07@hotmail.com>
2015-01-26 15:53:50 +0000

    refactored videos get and post methods after discussion on what we expected from the api for videos

--- routes/videos.js
+++ routes/videos.js
@@ -22,40 +22,60 @@ var Video = mongoose.model('Video', {
         index: true
     },
     duration:
-     {
+    {
         type: Number,
          lowercase: true
-     },
-    video_id:
-    {
-        type: String,
-        lowercase: true,
-        unique: true
-
     },
-    site_id:
+    ////video_id:
+    ////{
+    ////    type: String,
+    ////    lowercase: true
+    ////},
+    ////site_id:
+    ////{
+    ////    type: String,
+    ////    trim: true,
+    ////    lowercase: true,
+    ////    unique: true
+    ////},
+    url:
     {
         type: String,
-        trim: true,
-        lowercase: true,
-        unique: true
+        trim: true
     }
+
 });
+
+/**
+ * @swagger
+ * path: /video
+ * operations:
+ *   -  httpMethod: GET
+ *      summary: Get backs a list videos
+ *      notes: These include Site ID, Duration etc
+ *      nickname: Videos
+ *      consumes:
+ *        - application/json
+ *      parameters:
+ *        - name: username
+ *          description: Your username
+ *          paramType: query
+ *          required: true
+ *          dataType: string
+ *        - name: password
+ *          description: Your password
+ *          paramType: query
+ *          required: true
+ *          dataType: string
+ */
 
 router.get("/:video_id", function (req, res, next) {
     var query = Video.where({_id: req.params.video_id });
     query.findOne(function (err, video) {
         if(err) {
-            if(err.name == 'CastError'){
                 next(Boom.notFound("Video not found for this particular ID " + req.params.video_id));
-            }
         } else {
-            res.send({
-                id: video._id,
-                duration: video.duration,
-                site_id: video.site_id,
-                video_id: video.video_id
-            });
+            res.send(video);
         }
     });
 });
@@ -64,20 +84,13 @@ router.post("/", function (req, res, next) {
         console.log(JSON.stringify(req.body) + "\n\n");
         var video = new Video({
             title: req.body.title,
-                duration:req.body.duration,
-                    site_id: req.body.site_id,
-                        video_id: req.body.video_id
+                        url: req.body.url
         });
     video.save(function (err) {
             if (err){
                     next(err);
                 } else {
-                    res.send({
-                        title: req.body.title,
-                            duration: req.body.duration,
-                                siteid: req.body.site_id,
-                                    video_id: req.body.video_id
-                    });
+                    res.send("Video has been posted. Thanks");
             }
     });
 });
