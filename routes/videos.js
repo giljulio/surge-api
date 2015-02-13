1e166628b0aa52a6e3b8b3cf36091449fdf2c570
Qasim Shabir <Qasim-786-07@hotmail.com>
2015-02-13 11:19:54 +0000

    changes to controversial sort

--- routes/videos.js
+++ routes/videos.js
@@ -265,12 +265,18 @@ function standardDeviation(values){
 
     var stdDev = Math.sqrt(avgSquareDiff);
 
-    if (avg > 1000) {
-        stdDev += 1;
+    if(avg > 10000){
+        stdDev += 0.1;
+    } else if (avg > 5000){
+        stdDev += 0.2;
+    } else if (avg > 1000){
+        stdDev += 0.3;
+    } else if (avg > 500){
+        stdDev += 0.4;
     } else if (avg > 100){
-        stdDev += 2;
+        stdDev += 0.5;
     } else if (avg >= 0){
-        stdDev += 3;
+        stdDev += 0.6;
     }
     console.log("Stander dev after: " + stdDev);
     return stdDev;
